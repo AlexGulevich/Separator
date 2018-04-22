c4e5d0da6fdcd843414ca0fdefa307441e7f45d1
AlexGulevich <38635165+AlexGulevich@users.noreply.github.com>
2018-04-23 01:20:32 +0300

    Add files via upload

---
+++ unmix.py
@@ -0,0 +1,18 @@
+
+import numpy as np
+from scipy.io import wavfile
+
+
+# Get the recorded signals.
+freq1, data1 = wavfile.read('wav/mic1.wav')
+freq2, data2 = wavfile.read('wav/mic2.wav')
+
+# Assemble into a 2D array. Each row is a recording from a mic.
+x = np.vstack((data1, data2))
+
+#just to test
+y = x
+
+# Make new wav files containing the unmixed signals.
+wavfile.write('wav/unmix1.wav', freq1, y[0].astype(np.int16))
+wavfile.write('wav/unmix2.wav', freq1, y[1].astype(np.int16))
