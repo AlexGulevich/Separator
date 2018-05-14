85581093146027e58dfd5b6f2011fa1698d9644a
AlexGulevich <38635165+AlexGulevich@users.noreply.github.com>
2018-05-14 03:36:34 +0300

    New comments

--- unmix.py
+++ unmix.py
@@ -18,35 +18,41 @@ def check_not_exist(name):
 
 def mserr(x, y):
 
+    # center data
     x = x - x.mean()
     y = y - y.mean()
 
-    x = x / np.std(x)
+    # lead to unit variance
+    x = x / np.std(x)  # todo: what if zero variance?
     y = y / np.std(y)
 
+    # MSE itself
     return (( x - y )**2).mean()
     
 
 def minMSE(zz, data):
 
+    # unmixed data can be inverted
     mse1 = mserr(  zz[0, :], data)
     mse2 = mserr( -zz[0, :], data)
     mse3 = mserr(  zz[1, :], data)
     mse4 = mserr( -zz[1, :], data)
 
+    # also unmixed data can be swapped every frame
     mses = [mse1, mse2, mse3, mse4]
     mse = min(mses)
-    numb1 = np.argmin(mses)
+    numb = np.argmin(mses)
 
-    if   numb1 == 0:
+    if   numb == 0:
         zz1 =  zz[0, :]
-    elif numb1 == 1:
+    elif numb == 1:
         zz1 = -zz[0, :]
-    elif numb1 == 2:
+    elif numb == 2:
         zz1 =  zz[1, :]
-    elif numb1 == 3:
+    elif numb == 3:
         zz1 = -zz[1, :]
 
+    # mse and choosed unmixed channel
     return mse, zz1
 
 
@@ -72,26 +78,29 @@ def split(name1, name2, uxname1, uxname2):
     x = np.vstack((data1, data2))
 
 
-    step =   400
-    maxv = 50000
-    minv =  5000   #6400
-    overlap = 0.25   # from 0.0 to 0.5
+    frame_overlap = 0.25    # from 0.0 to 0.99
+    frame_step =    800     # step:            frame_overlap * frame_step should be int
+    frame_maxv = 100000     # frame max value 
+    frame_minv =   4000     # frame min value: frame_minv    * frame_step should be int
+    
 
     res  = []
     res2 = []
     time = []
 
-    for frame in range(minv, maxv + 1, step):
+    for frame in range(frame_minv, frame_maxv + 1, frame_step):
 
         mse1 = 0
         mse2 = 0
-        zzz = np.array([[],[]])
+        zzz = [[],[]]
 
-        for k in range(1, math.floor(( len(data1) - frame ) / ( frame * ( 1 - overlap ) ) ) + 1 + 1):
+        for k in range(1, math.floor(( len(data1) - frame ) / ( frame * ( 1 - frame_overlap ) ) ) + 1 + 1):
 
-            xn1 = round( ( k - 1) * frame * ( 1 - overlap ) )
+            # indices for xx
+            xn1 = round( ( k - 1) * frame * ( 1 - frame_overlap ) )
             xn2 = xn1 + frame
 
+            # make frame
             xx = x[:, xn1:xn2]
             
             # Do kICA.
@@ -100,71 +109,37 @@ def split(name1, name2, uxname1, uxname2):
             # Unmix the signal.
             zz = np.dot(w, yy)
 
-            zn1 = round( frame * (     overlap / 2 ) )
-            zn2 = round( frame * ( 1 - overlap / 2 ) )
+            # indices for zz
+            zn1 = round( frame * (     frame_overlap / 2 ) )
+            zn2 = round( frame * ( 1 - frame_overlap / 2 ) )
 
+            # indices for input data
             dn1 = xn1 + zn1
             dn2 = xn1 + zn2
 
+            # mse and choose unmixed data
             mse1_frame, zz1 = minMSE(zz[:, zn1:zn2], data1[dn1:dn2])
             mse2_frame, zz2 = minMSE(zz[:, zn1:zn2], data2[dn1:dn2])
             
+            #frame MSE
             mse1 = mse1 + mse1_frame
             mse2 = mse2 + mse2_frame
 
-            zzz = np.append(zzz, 1000*np.vstack((zz1, zz2)), 1)
+            # unmixed data
+            zzz = np.append(zzz, np.vstack((zz1, zz2)), 1)
 
-##            mse11 = mserr(  zz[0, zn1:zn2], data1[dn1:dn2])
-##            mse12 = mserr( -zz[0, zn1:zn2], data1[dn1:dn2])
-##            mse13 = mserr(  zz[1, zn1:zn2], data1[dn1:dn2])
-##            mse14 = mserr( -zz[1, zn1:zn2], data1[dn1:dn2])
-##
-##            mse21 = mserr(  zz[0, zn1:zn2], data2[dn1:dn2])
-##            mse22 = mserr( -zz[0, zn1:zn2], data2[dn1:dn2])
-##            mse23 = mserr(  zz[1, zn1:zn2], data2[dn1:dn2])
-##            mse24 = mserr( -zz[1, zn1:zn2], data2[dn1:dn2])
-##
-##
-##            mses = [mse11, mse12, mse13, mse14]
-##            mse1_ = min(mses)
-##            numb1 = np.argmin(mses)
-##
-##            mses = [mse21, mse22, mse23, mse24]
-##            mse2_ = min(mses)
-##            numb2 = np.argmin(mses)
-##            
-##            mse1 = mse1 + mse1_
-##            mse2 = mse2 + mse2_
-##
-##            if   numb1 == 0:
-##                zz1 =  zz[0, zn1:zn2]
-##            elif numb1 == 1:
-##                zz1 = -zz[0, zn1:zn2]
-##            elif numb1 == 2:
-##                zz1 =  zz[1, zn1:zn2]
-##            elif numb1 == 3:
-##                zz1 = -zz[1, zn:zn2]
-##
-##            if   numb2 == 0:
-##                zz2 =  zz[0, zn1:zn2]
-##            elif numb2 == 1:
-##                zz2 = -zz[0, zn1:zn2]
-##            elif numb2 == 2:
-##                zz2 =  zz[1, zn1:zn2]
-##            elif numb2 == 3:
-##                zz2 = -zz[1, zn1:zn2]
-
-            
-            
-
+        #frame MSE
         res .append(math.sqrt( (mse1 + mse2) / 2 ))
 
+        # global MSE
         mse1 = mserr( zzz[0, :], data1[zn1 : zn1 + len(zzz[0, :])])
         mse2 = mserr( zzz[1, :], data2[zn1 : zn1 + len(zzz[1, :])])
         res2.append(math.sqrt( (mse1 + mse2) / 2 ))
-        
+
+        # X axis
         time.append(frame / freq1);
 
+    #plots
     plt.subplot(2,1,1)
     plt.plot(time, res )
     plt.ylabel("Frame MSE")
@@ -176,9 +151,10 @@ def split(name1, name2, uxname1, uxname2):
     plt.ylabel("Global MSE")
     
     plt.show()
+    
     # Make new wav files containing the unmixed signals.
-    wavfile.write(uxname1, freq1, zzz[0].astype(np.int16))
-    wavfile.write(uxname2, freq1, zzz[1].astype(np.int16))
+    wavfile.write(uxname1, freq1, 1000*zzz[0].astype(np.int16))
+    wavfile.write(uxname2, freq1, 1000*zzz[1].astype(np.int16))
     
 
 def merge (name1, name2, xname1, xname2, mix = np.array([1,0,0,1])):
@@ -237,6 +213,7 @@ if __name__=='__main__':
     #    merge(args.in1, args.in2, args.out1, args.out2, np.array(args.mix_matrix).astype(np.float32))
 
     merge("wav/Alice_8k_8bit.wav", "wav/George_8k_8bit.wav", "wav/GA1.wav", "wav/GA2.wav", np.array([0.7, 0.3, 0.3, 0.7]).astype(np.float32))
+    #merge("wav/OSR_fr_000_0041_8k.wav", "wav/OSR_us_000_0010_8k.wav", "wav/GA1.wav", "wav/GA2.wav", np.array([0.7, 0.3, 0.3, 0.7]).astype(np.float32))
 
     split("wav/GA1.wav", "wav/GA2.wav", "wav/ouuu1.wav", "wav/ouuu2.wav")
     
